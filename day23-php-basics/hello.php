9b79e1715726024a8460f70f0c4f1a0db6a23373
j_polak <jan.polak@onmyway.cz>
2021-02-03 17:57:22 +0100

    day 23 php basics

---
+++ day23-php-basics/hello.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php
+
+1 + 1;
+
+$my_greeting = "Hello, world!";
+
+echo $my_greeting;
+
+var_dump($my_greeting);
+
+$number = 0;
+$number++; // 1
+var_dump($number);
+
+var_dump($number++);
+
+var_dump($number);
+
+var_dump(++$number);
+
+?>
+
+</body>
+</html>
